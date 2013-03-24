985c61e57d3fa1bf5e7a1b6e5fd3575946bf8808
Andrew8xx8 <avk@8xx8.ru>
2013-03-25 02:18:03 +0400

    Personal snippet model added

--- spec/factories.rb
+++ spec/factories.rb
@@ -212,6 +212,13 @@ FactoryGirl.define do
     file_name
   end
 
+  factory :personal_snippet do
+    author
+    title
+    content
+    file_name
+  end
+
   factory :snippet do
     author
     title
